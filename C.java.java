2e1e543e922538b142f55c6ffcdde85a143f68de
Mohitt126 <90523986+Mohitt126@users.noreply.github.com>
2021-09-18 13:50:08 +0530

    Add files via upload

---
+++ C.java.java
@@ -0,0 +1,17 @@
+package Geekster;
+
+	import java.util.Scanner;
+	public class c {
+		public static void main(String [] args) {
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter a number");
+			int i=scan.nextInt();
+			long a=i;
+			float f=a;
+
+			System.out.println("Int value "+i); 
+	        System.out.println("Float value" +f); 
+	        scan.close();
+		}
+	}
+
